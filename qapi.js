c38ad7fa4c5e6bcb8d270c44250f2dc8a24d7201
Sulin Alexander <50768680+Wurikos@users.noreply.github.com>
2021-02-26 17:41:47 +0300

    delete page 2

---
+++ qapi.js
@@ -0,0 +1,35 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6039032b465b2a001e0eac63",
+				"6039032b465b2a001e0eac65",
+				"6039082e83b4ad001e35304c"
+			]
+		},
+		"6039032b465b2a001e0eac63": {
+			"id": "6039032b465b2a001e0eac63",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6039032b465b2a001e0eac65": {
+			"id": "6039032b465b2a001e0eac65",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"6039082e83b4ad001e35304c": {
+			"id": "6039082e83b4ad001e35304c",
+			"pageUrl": "page1",
+			"name": "Page1"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
